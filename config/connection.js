e4e27466b846f86fa4844815b1796305263994a6
sarathrajan922 <sarathrajan922@gmail.com>
2023-04-08 16:04:41 +0530

    db updation

--- config/connection.js
+++ config/connection.js
@@ -6,13 +6,13 @@ const state ={
 }
 
 module.exports.connect = (done)=>{
-    const url = mongoDbUrl
-    const dbname =  'shop'
+    const url = 'mongodb+srv://sarathrajan:cnbvRjsjfyQA90Se@timeshub.l0vdmu5.mongodb.net/test'
+    const dbname = 'timesHub'
 
     MongoClient.connect(url, async(err,data)=>{
-        if(err)
+        if(err) 
         return done(err)
-        state.db=await data.db(dbname)
+        state.db=await data.db(dbname)      
         done()
     })
 }
