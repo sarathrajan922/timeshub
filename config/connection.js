ec5b21ad68dace4a97acbad25330100957d03957
sarathrajan922 <sarathrajan922@gmail.com>
2023-04-11 01:18:36 +0530

    db connection

--- config/connection.js
+++ config/connection.js
@@ -6,10 +6,10 @@ const state ={
 }
 
 module.exports.connect = (done)=>{
-    // const url = 'mongodb+srv://sarathrajan:cnbvRjsjfyQA90Se@timeshub.l0vdmu5.mongodb.net/test'
-    const url = 'mongodb://0.0.0.0:27017'
-    // const dbname = 'timesHub'
-    const dbname = 'shop'
+    const url = 'mongodb+srv://sarathrajan:cnbvRjsjfyQA90Se@timeshub.l0vdmu5.mongodb.net/test'
+    // const url = 'mongodb://0.0.0.0:27017'
+    const dbname = 'timesHub'
+    // const dbname = 'shop'
 
     MongoClient.connect(url, async(err,data)=>{
         if(err) 
